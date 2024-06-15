832a1ef148959530260cb8add4790e0df5b9d079
MacCandy <devisb@mail.ru>
2024-06-15 12:54:20 +0300

    new

--- document.addEventListener('DOMContentLoa.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const showGraphBtn = document.getElementById('showGraphBtn');
-    const graphDisplay = document.getElementById('graphDisplay');
-
-    showGraphBtn.addEventListener('click', function() {
-        // Здесь можно добавить логику для отображения графики
-        graphDisplay.innerHTML = '<img src="https://example.com/graph_image.jpg" alt="Графика">';
-    });
-});
